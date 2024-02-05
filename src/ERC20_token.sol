3ad4d6c4feb69ae942cb3d7fa4afa69d25f8607b
mrthedude <mrthedude429@gmail.com>
2024-02-05 11:54:07 -0500

    added a remappings.txt with a more complete path for OpenZeppelin files and removed the remapping for OpenZeppelin files in my foundry.toml, now ERC20 tokens are verifying on ScrollScan

--- src/ERC20_token.sol
+++ src/ERC20_token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract token is ERC20 {
-    constructor() ERC20("Level4", "LVL4") {
+    constructor() ERC20("Level4", "LVL_4") {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
