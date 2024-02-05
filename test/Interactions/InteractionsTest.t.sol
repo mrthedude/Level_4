ad75d61947fac8df36863f5d96374f403d002e14
mrthedude <mrthedude429@gmail.com>
2024-02-05 12:55:00 -0500

    wrote 2 integration tests verifying the deployment of the advancedLending contract and the token contract

--- test/Interactions/InteractionsTest.t.sol
+++ test/Interactions/InteractionsTest.t.sol
@@ -2,15 +2,32 @@
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Vm} from "forge-std/Vm.sol";
-import {StdCheats} from "forge-std/StdCheats.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {testAdvancedLendingDeployer} from "../../script/AdvancedLendingDeployment.s.sol";
 import {AdvancedLending} from "../../src/AdvancedLending.sol";
+import {token} from "../../src/ERC20_token.sol";
 
-contract InteractionsTest is StdCheats, Test {
+contract InteractionsTest is Test, testAdvancedLendingDeployer {
     AdvancedLending public advancedLending;
+    token public myToken;
     HelperConfig public helperConfig;
+    address public contractOwner;
+    uint256 public STARTING_USER_BALANCE = 10 ether;
 
-    function setUp() external {}
+    function setUp() external {
+        testAdvancedLendingDeployer deployer = new testAdvancedLendingDeployer();
+        contractOwner = address(deployer);
+        (advancedLending, myToken) = deployer.run();
+    }
+
+    function test_contractAndTokenDeployment() public {
+        assertEq(myToken.balanceOf(contractOwner), 100000e18);
+    }
+
+    function test_tokenDeploymentMatchesTokenContract() public {
+        address tokenContract = advancedLending.getTokenAddress();
+        address tokenDeployed = address(myToken);
+        console.log(tokenContract, tokenDeployed);
+        assertEq(tokenContract, tokenDeployed);
+    }
 }
