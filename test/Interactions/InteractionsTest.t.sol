b03110c9f621cea8017e2b12e758f7905e2ec901
mrthedude <mrthedude429@gmail.com>
2024-02-05 11:58:16 -0500

    added import for foundry vm functionality in tests

--- test/Interactions/InteractionsTest.t.sol
+++ test/Interactions/InteractionsTest.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {testAdvancedLendingDeployer} from "../../script/AdvancedLendingDeployment.s.sol";
