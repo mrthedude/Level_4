e3643900d6876f14117c39925bff19418a25c306
mrthedude <mrthedude429@gmail.com>
2024-02-07 14:09:23 -0500

    edited file

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -42,4 +42,16 @@ contract HelperConfig is Script {
 
         anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
     }
+
+    function getOwnerAddress() public view returns (address _owner) {
+        if (block.chainid == 534351) {
+            // Scroll Sepolia
+            return 0x6a571992ECaaDe9df63334BACEdD46C7C78e3Ef9;
+        }
+
+        if (block.chainid == 31337) {
+            // Anvil
+            return 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+        }
+    }
 }
