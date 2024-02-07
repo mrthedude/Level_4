f6feb64615c0c09eea9704704fe770ebe5612235
mrthedude <mrthedude429@gmail.com>
2024-02-07 13:21:52 -0500

    added Ownable funcitonality from OpenZeppelin library

--- src/ERC20_token.sol
+++ src/ERC20_token.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract token is ERC20 {
-    constructor() ERC20("Level4", "LVL_4") {
+contract token is ERC20, Ownable {
+    constructor(address owner) ERC20("Level4", "LVL_4") Ownable(owner) {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
